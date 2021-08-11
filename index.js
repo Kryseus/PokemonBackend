8bcff9fc633700313608770c86c4fe02d0b07c35
Philip Tronser <tronser@web.de>
2021-08-11 16:03:46 +0200

    cors added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import pokeRouter from "./router/pokeRouter.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors({ origin: "*" }));
 
 //app.get("/pokemon", (req, res) => res.send(pokeData));
-app.use('/pokemon', pokeRouter);
+app.use("/pokemon", pokeRouter);
 /* app.get('/pokemon/:id', (req, res) => {
   console.log(req.params.id);
   res.send();
@@ -15,5 +17,3 @@ app.use('/pokemon', pokeRouter);
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
-
-
