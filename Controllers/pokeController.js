e22c14bf25754133c8e43643ca08780865b192bb
Philip Tronser <tronser@web.de>
2021-08-12 21:15:38 +0200

    pokemon ready

--- Controllers/pokeController.js
+++ Controllers/pokeController.js
@@ -3,11 +3,12 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const pokeData = require("../db/Pokemon.json");
 
-export const getAll = (req, res) =>  res.status(200).json(pokeData);
+export const getAll = (req, res) => res.status(200).json(pokeData);
 
 export const getSingle = (req, res) => {
- const { id } = req.params;
- const pokemon = pokeData.find(pokemon => pokemon.id === Number(id))
- if(!pokemon) res.status(404).json({error: `Pokemon with id of ${id} does not exist`})
- res.status(200).json(pokemon)
+  const { id } = req.params;
+  const pokemon = pokeData.find((pokemon) => pokemon.id === Number(id));
+  if (!pokemon)
+    res.status(404).json({ error: `Pokemon with id of ${id} does not exist` });
+  res.status(200).json(pokemon);
 };
